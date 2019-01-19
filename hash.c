9eedec6df86b42f7f07f560480f197ef51bc6ffa
ryan p <ryan.eliopoulos@gmail.com>
2019-01-19 11:04:58 -0800

    finished newTable()

--- hash.c
+++ hash.c
@@ -13,9 +13,19 @@ HashTable *newTable(int (*entryCmpr)(HashEntry*, HashEntry*)) {
     new_table->highest_collision_count = 0;
     new_table->total_collisions = 0;
     new_table->entryCompareFnx = entryCmpr; 
-
+    new_table->table_size = INITIAL_TABLE_SIZE;
+    new_table->table_directory[INITIAL_TABLE_SIZE];   
+ 
+    for (int i = 0; i < INITIAL_TABLE_SIZE; i++) {
+        new_table->table_directory[i] == NULL;
+    }
     return new_table; 
 }
 
 
+void addEntry(HashTable *hash_table, HashEntry *new_entry) {
 
+    // unsigned long long int hash = hash(new_entry->hash_field) % hash_table->table_size;
+
+    
+}
