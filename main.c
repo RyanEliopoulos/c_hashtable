f4038f022c69eb53d64298d3a943c5860211738c
ryan p <ryan.eliopoulos@gmail.com>
2019-01-19 10:42:49 -0800

    more stuff

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-#include"parseArgs.h"
 
+/* Remember to assert after every malloc */
+/* Probably should go through the code and change ints to unsigned long, etc. in certain places. Don't want overflow */
+
+#include"parseArgs.h"
+#include"hash.h"
 
 void main(int argc, char* argv[]) {
     
     int count = parseArgs(argc, argv);
     printf("Count is :%d\n", count);
+    HashTable *new_table = newTable();
+
+    printf("done\n");
 }
