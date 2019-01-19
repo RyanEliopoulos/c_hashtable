57c6a07e947eda4f96360f31b17efe21c8a49d68
ryan p <ryan.eliopoulos@gmail.com>
2019-01-19 12:04:48 -0800

    ready to begin testing

--- hash.c
+++ hash.c
@@ -1,7 +1,5 @@
 #include"hash.h"
-
-
-
+#include"crc64.h"
 
 HashTable *newTable(int (*entryCmpr)(HashEntry*, HashEntry*)) {
 
@@ -23,9 +21,48 @@ HashTable *newTable(int (*entryCmpr)(HashEntry*, HashEntry*)) {
 }
 
 
-void addEntry(HashTable *hash_table, HashEntry *new_entry) {
+void addEntry(HashTable *hash_table, Data *data_entry) {
 
-    // unsigned long long int hash = hash(new_entry->hash_field) % hash_table->table_size;
-
+    unsigned long long int hash = crc64(data_entry->hash_field) % hash_table->table_size; // Calculating hash value
     
+    HashEntry *new_entry = newTableEntry(data_entry); // Creating an entry node for the hash table
+
+    if (hash_table->table_directory[hash] == NULL) { // Bucket is empty, assign and return
+        hash_table->table_directory[hash] = new_entry;
+        return;
+    } 
+
+    /* Bucket is not empty */
+    /* Will iterate through bucket list, incrementing a matching entry or adding the new unique entry to the end. */
+    HashEntry *temp_entry =  hash_table->table_directory[hash];
+
+    while ( temp_entry->next_node != NULL ) {
+
+        /* Comparing HashEntry values */
+        if ( (*hash_table->entryCompareFnx)(temp_entry, new_entry) ) {  // True if entries match.
+            temp_entry->occurrences++; 
+            return;
+        }
+        temp_entry = temp_entry->next_node; 
+    }
+   
+    /* Checking the last value in the list */
+    if ( (*hash_table->entryCompareFnx)(temp_entry, new_entry) ) {
+        temp_entry->occurrences++;
+    }
+    else {
+       temp_entry->next_node = new_entry; // new_entry is unique to the hash table
+    }
+}
+
+
+HashEntry *newTableEntry(Data *data_entry) {    
+
+    /* Create and initialize new HashEntry */ 
+    HashEntry *new_entry = malloc(sizeof(HashEntry));
+    new_entry->data = data_entry;
+    new_entry->next_node = NULL;
+    new_entry->occurrences = 0;
+
+    return new_entry;
 }
