50d3c4d5aea6f756306eb1c609180f15c65fb3a3
ryan p <ryan.eliopoulos@gmail.com>
2019-01-24 19:42:26 -0800

    +print function

--- main.c
+++ main.c
@@ -5,36 +5,42 @@
 #include"parseFiles.h"
 #include<stdlib.h>
 
+
+void printWords(int, HashEntry **);
+
 void main(int argc, char* argv[]) {
     
     int count = parseArgs(argc, argv);
     printf("Count is :%d\n", count);
 
-    
     HashTable *hash_table = parseFiles(count, argc, argv);
-
-
     HashEntry **sorted_entries = sortPairs(hash_table);
 
+    /* No user-specified count value, so default to printing all pairs */
+    if (count == -1) count = hash_table->unique_entries;
+
+    printWords(count, sorted_entries);
+    /* Printing out "sorted" word pairs */
+    /*
     char *string1;
     char *string2;  
-    for (int i = 0; i < hash_table->unique_entries; i++ ) {
+    //for (int i = 0; i < hash_table->unique_entries; i++ ) {
+    for (int i = 0; i < count; i++ ) {
         string1 = sorted_entries[i]->data->string1;
         string2 = sorted_entries[i]->data->string2;
         printf("<%s>, <%s>, %llu\n", string1, string2, sorted_entries[i]->occurrences);
 
-    } 
+    } */
     //HashEntry **unpacked_entry_array = unpackTableEntries(hash_table);
 
     // This is where the sortPairs() call goes
     // sortPairs(hash_table); // calls unpackTableEntries and qsort()
-
     // BEGIN: these three lines free all elements of the table and sub-structures.
     /* So, the logc within the unpackTableEntries() is likely causing the segfault */
     debug_traverseTable(hash_table);
     free(hash_table->table_directory);
     free(hash_table);
-    //free(unpacked_entry_array);
+    free(sorted_entries);
     // END
     
     // DEBUG - should be getting a hash table with complete set of values.
@@ -63,4 +69,23 @@ void main(int argc, char* argv[]) {
     //free(hash_table->table_directory);
     //free(hash_table);
     //printf("total entries:%llu\n", hash_table->total_entries);
+}
+
+
+void printWords(int count, HashEntry **sorted_array) {
+
+    for (int i = 0; i < count; i++) {
+            
+        /* retrieve the entry's string values */ 
+        char *string1 = sorted_array[i]->data->string1;
+        char *string2 = sorted_array[i]->data->string2;        
+     
+        /* Transfer the strings into a single string */
+        char final_string[DICT_MAX_WORD_LEN * 2];
+        strcpy(final_string, string1);
+        strcat(final_string, string2);
+        
+        printf("%10d %s\n", sorted_array[i]->occurrences, final_string);
+            
+    }
 }
