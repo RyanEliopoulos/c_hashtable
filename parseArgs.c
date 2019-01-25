c2bdc5846cb2d3bd69b60049be07335d9ad78545
ryan p <ryan.eliopoulos@gmail.com>
2019-01-24 20:35:41 -0800

    +comments

--- parseArgs.c
+++ parseArgs.c
@@ -1,10 +1,13 @@
 #include"parseArgs.h"
 
-
-/* ADD MORE GENERIC ERROR MESSAGES */
-
-/* Ensures the arguments passed at the command line make sense. */
-/* <count> must be positive. Filepaths are checked for validity. */
+/*
+ *  This function screens the command-line arguments passed to the program to ensure
+ *  smooth operation and offer guidance on use as needed.
+ *
+ *  <count> must be an integer greater than zero and at least one filepath must be supplied.
+ *  Program will fail if any of the supplied filepaths are invalid. 
+ *  An exit code of 1XX indicates the program has failed this initial screen.
+ */ 
 int parseArgs(int argc, char *argv[]) {
 
     /* Checking for insufficient command-line arguments */
--- parseArgs.h
+++ parseArgs.h
@@ -1,8 +1,23 @@
+/*  Ryan Paulos
+ *  CS 360 
+ *  Spring 2019
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 
 /* For checking file paths */ 
 #include<sys/stat.h>
 #include<unistd.h>
+
+/*
+ *  This function screens the command-line arguments passed to the program to ensure
+ *  smooth operation and offer guidance on use as needed.
+ *
+ *  <count> must be an integer greater than zero and at least one filepath must be supplied.
+ *  Program will fail if any of the supplied filepaths are invalid. 
+ *  An exit code of 1XX indicates the program has failed this initial screen.
+ *
+ */
 
 int parseArgs(int, char**);
