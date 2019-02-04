2fa5ac6faa920a95eccfccb56b7a634354a150d6
ryan p <ryan.eliopoulos@gmail.com>
2019-02-03 20:36:34 -0800

    Revert "everyone needs a head(er)"
    This reverts commit 38b532a1a7da72da3dc1aedac29dabd8e00663b7.

--- main.h
+++
@@ -1,7 +0,0 @@
-#include"parseArgs.h"
-#include"parseFiles.h"
-#include<stdlib.h>
-
-/* traverses a sorted array of table entries */
-/* prints as many pairs as specified         */
-void printWords(int, HashEntry **);
